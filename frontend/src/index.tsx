e24528d8c2b63645496ab2b3243bd015a99c5b89
lj05117 <lj05117@naver.com>
2023-04-19 10:32:36 +0900

    initial commit

---
+++ frontend/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./store/index";
+import { Provider } from "react-redux";
+//사가 불러오는 법 알아보기. ver18에서
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
---
+++ frontend/src/store/index.ts
@@ -0,0 +1,39 @@
+import createSagaMiddleware from "redux-saga";
+import { all } from "redux-saga/effects";
+import authReducer from "./authSlice";
+
+// 스토어 통합관련(리듀서들, 사가들)
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+// saga 관리
+
+// 관리하는 슬라이스들
+
+// rootReducers by using combineReducers
+const rootReducers = combineReducers({ auth: authReducer });
+
+// rootSaga
+function* rootSaga() {
+  //   yield all();
+}
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+// Store 구성 -> configureStore
+// 여러가지 리듀서를 하나로 합칠 수 있음
+// 왜냐면 store는 리듀서 하나만 가져야되서 합침
+const store = configureStore({
+  // 여러 슬라이서의 리듀서를 합침
+  reducer: rootReducers,
+  middleware: middlewares,
+});
+
+sagaMiddleware.run(rootSaga);
+
+// 외부에서 쓰려고
+export default store;
+
+// react-redux 라이브러리를 깔면
+// 귀찮게 subscribe 함수 쓸 필요없음
+// import { createStore } from 'redux'
