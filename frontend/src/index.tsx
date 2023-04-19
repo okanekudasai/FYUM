4ff3e051ea499fc604ba2db94ae5c4cd5ca615ca
lj05117 <lj05117@naver.com>
2023-04-19 15:08:31 +0900

    feat: css 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./store/index";
 import { Provider } from "react-redux";
+import "./index.css";
 //사가 불러오는 법 알아보기. ver18에서
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
