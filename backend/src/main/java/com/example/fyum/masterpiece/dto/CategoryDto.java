588467da45726ad6c3ac6683a10612fe74301e08
임유정 <melimelame@gmail.com>
2023-04-21 15:27:12 +0900

    feat: add CategoryDto
    - 명화 조회 카테고리 정보 반환용 Dto

--- backend/src/main/java/com/example/fyum/masterpiece/dto/CategoryDto.java
+++ backend/src/main/java/com/example/fyum/masterpiece/dto/CategoryDto.java
@@ -3,5 +3,17 @@ package com.example.fyum.masterpiece.dto;
 import com.example.fyum.masterpiece.entity.Painter;
 
 public class CategoryDto {
+    int id;
+    String nameKr;
+    String nameOrigin;
+    String imgSrc;
+
+    public CategoryDto(Painter painter) {
+        this.id = painter.getId();
+        this.nameKr = painter.getPainterKr();
+        this.nameOrigin = painter.getPainterOrigin();
+        this.imgSrc = painter.getImgSrc();
+    }
+
 
 }
