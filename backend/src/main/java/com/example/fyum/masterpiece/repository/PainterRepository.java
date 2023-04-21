5a6784b877a2ca1cfddb8a9bc831aedc379a9f4b
임유정 <melimelame@gmail.com>
2023-04-21 15:26:25 +0900

    feat: add PainterRepository
    - 화가 정보 조회용 repository 생성

--- backend/src/main/java/com/example/fyum/masterpiece/repository/PainterRepository.java
+++ backend/src/main/java/com/example/fyum/masterpiece/repository/PainterRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PainterRepository extends JpaRepository<Painter, Integer> {
-
+    Page<Painter> findAll(Pageable pageable);
 
 }
