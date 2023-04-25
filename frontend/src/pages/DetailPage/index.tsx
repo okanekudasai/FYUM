65add664a7da7940dba9b176b3e3f0e4efa87bb4
ahyunnnnnn <ahyun3089@naver.com>
2023-04-25 15:25:14 +0900

    feat: description on/off 버튼 기능 구현

--- frontend/src/pages/DetailPage/index.tsx
+++ frontend/src/pages/DetailPage/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   DetailContainer,
@@ -8,6 +9,8 @@ import {
   GoBackBtn,
   LeftArrowIcStyle,
   GoBackBtnContent,
+  DescriptionBtn,
+  DescriptionP,
 } from "./styles";
 import detailImg from "../../assets/dummyImg/starrynight.png";
 
@@ -16,23 +19,39 @@ const DetailPage = () => {
   const moveToList = () => {
     navigate("/artlist");
   };
+
+  const [description, setDescription] = useState(true);
+
+  const changeState = () => {
+    setDescription(!description);
+  };
+  console.log(description);
   return (
     <DetailContainer>
-      <BackgroundImg src={detailImg} />
-      <ContentContainer>
-        <Title>The Starry Night</Title>
-        <Content>
-          《별이 빛나는 밤》(영어: The Starry Night)은 네덜란드의 화가 빈센트 반
-          고흐의 가장 널리 알려진 작품이자 정신병을 앓고 있을 당시 고흐가 그린
-          그림이다. 1889년 생레미의 정신병원에서 고흐는 정신적 질환으로 인한
-          고통을 떠올려 그림 속의 소용돌이로 묘사했다.
-        </Content>
-        <GoBackBtn onClick={moveToList}>
-          <LeftArrowIcStyle />
-          &nbsp;&nbsp;&nbsp;
-          <GoBackBtnContent>Go Back</GoBackBtnContent>
-        </GoBackBtn>
-      </ContentContainer>
+      <BackgroundImg src={detailImg} description={description} />
+      {description === true ? (
+        <ContentContainer>
+          <Title>The Starry Night</Title>
+          <Content>
+            《별이 빛나는 밤》(영어: The Starry Night)은 네덜란드의 화가 빈센트
+            반 고흐의 가장 널리 알려진 작품이자 정신병을 앓고 있을 당시 고흐가
+            그린 그림이다. 1889년 생레미의 정신병원에서 고흐는 정신적 질환으로
+            인한 고통을 떠올려 그림 속의 소용돌이로 묘사했다.
+          </Content>
+          <GoBackBtn onClick={moveToList}>
+            <LeftArrowIcStyle />
+            &nbsp;&nbsp;&nbsp;
+            <GoBackBtnContent>Go Back</GoBackBtnContent>
+          </GoBackBtn>
+        </ContentContainer>
+      ) : null}
+      <DescriptionBtn onClick={changeState}>
+        {description === true ? (
+          <DescriptionP>Description On.</DescriptionP>
+        ) : (
+          <DescriptionP>Description Off.</DescriptionP>
+        )}
+      </DescriptionBtn>
     </DetailContainer>
   );
 };
--- frontend/src/pages/DetailPage/styles.ts
+++ frontend/src/pages/DetailPage/styles.ts
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import { black, white } from "../../styles/colors";
 import { ReactComponent as LeftArrowIc } from "../../assets/icon/leftArrowIc.svg";
 
+interface BackgroundImgProps {
+  src: string;
+  description: boolean; // description 프로퍼티 타입 정의
+}
+
 export const DetailContainer = styled.div`
   width: 100vw;
   height: 100vh;
@@ -10,12 +15,14 @@ export const DetailContainer = styled.div`
   /* overflow-y: hidden; */
 `;
 
-export const BackgroundImg = styled.img`
+export const BackgroundImg = styled.img<BackgroundImgProps>`
   width: 100vw;
   height: 100vh;
   position: absolute;
   object-fit: fill;
-  filter: brightness(50%);
+  filter: brightness(
+    ${(props) => (props.description === true ? "40%" : "100%")}
+  );
 `;
 
 export const ContentContainer = styled.div`
@@ -55,4 +62,19 @@ export const GoBackBtnContent = styled.p`
   font-size: 1.5vw;
   color: ${white};
   padding-bottom: 0.2%;
+`;
+
+export const DescriptionBtn = styled.div`
+  position: absolute;
+  bottom: 0.5vw;
+  left: 1.5vw;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const DescriptionP = styled.p`
+  color: ${white};
+  font-size: 1.5vw;
 `;
