ef48f07139dda33d70f1e57a03fa05c1809b14f5
ahyunnnnnn <ahyun3089@naver.com>
2023-04-28 15:03:25 +0900

    feat: 가로 스크롤 구현 중

--- frontend/src/pages/LikedListPage/index.tsx
+++ frontend/src/pages/LikedListPage/index.tsx
@@ -1,13 +1,44 @@
-import { LikedListContainer, TitleContainer, TitleP, TitleHr } from "./styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  LikedListContainer,
+  TitleContainer,
+  TitleP,
+  TitleHr,
+  SwiperSettings,
+  SwiperDiv,
+  SwiperImgDiv,
+  SwiperTitle,
+  SwiperImg,
+} from "./styles";
+// 더미 데이터
+import dummy from "../../assets/dummyImg/liked.json";
 
+interface DummyInfo {
+  paintingId: number;
+}
 const LikedListPage = () => {
+  const paintings = dummy.paintings;
+
   return (
     <LikedListContainer>
       <TitleContainer>
         <TitleP>Liked List</TitleP>
         <TitleHr />
       </TitleContainer>
+      <Swiper {...SwiperSettings}>
+        {paintings.map((paintings) => (
+          <SwiperSlide key={paintings.paintingId}>
+            <SwiperDiv>
+              <SwiperImgDiv>
+                <SwiperImg src={paintings.imcSrc} />
+              </SwiperImgDiv>
+              <SwiperTitle>{paintings.title}</SwiperTitle>
+            </SwiperDiv>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </LikedListContainer>
   );
 };
+
 export default LikedListPage;
--- frontend/src/pages/LikedListPage/styles.ts
+++ frontend/src/pages/LikedListPage/styles.ts
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import { black, white } from "../../styles/colors";
 
 export const LikedListContainer = styled.div`
@@ -14,6 +18,7 @@ export const TitleContainer = styled.div`
   margin-top: 15vh;
   margin-left: auto;
   margin-right: auto;
+  margin-bottom: 10vh;
   width: 30vw;
 `;
 
@@ -28,4 +33,34 @@ export const TitleP = styled.p`
 export const TitleHr = styled.hr`
   width: 50%;
   margin-top: 0;
+`;
+
+export const SwiperSettings = {
+  spaceBetween: 20,
+  scrollbar: {
+    draggable: true,
+  },
+  loop: true,
+  slidesPerView: 3,
+};
+
+export const SwiperTitle = styled.p`
+  color: ${white};
+  text-align: center;
+`;
+
+export const SwiperDiv = styled.div`
+  width: 25vw;
+  height: 50vh;
+`;
+
+export const SwiperImgDiv = styled.div`
+  vertical-align: center;
+  width: 100%;
+`;
+
+export const SwiperImg = styled.img`
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
 `;
