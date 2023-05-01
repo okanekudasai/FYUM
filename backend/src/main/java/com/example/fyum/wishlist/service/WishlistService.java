0956139297df399578836c120c24618d592c7f23
조재완 <johjaewan@naver.com>
2023-05-01 15:29:04 +0900

    feat: 찜하기 취소, 찜하기 목록

--- backend/src/main/java/com/example/fyum/wishlist/service/WishlistService.java
+++ backend/src/main/java/com/example/fyum/wishlist/service/WishlistService.java
@@ -1,15 +1,21 @@
 package com.example.fyum.wishlist.service;
 
+
 import com.example.fyum.config.Painting;
-import com.example.fyum.config.PaintingRepository;
+import com.example.fyum.masterpiece.entity.Masterpiece;
+import com.example.fyum.masterpiece.repository.MasterpieceRepository;
+import com.example.fyum.masterpiece.repository.PaintingRepository;
 import com.example.fyum.member.entity.Member;
 import com.example.fyum.member.repository.MemberRepository;
+import com.example.fyum.wishlist.dto.WishlistResponseDto;
 import com.example.fyum.wishlist.entity.Wishlist;
 import com.example.fyum.wishlist.repository.WishlistRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,18 +23,21 @@ import java.util.Optional;
 public class WishlistService {
 
     private final MemberRepository memberRepository;
-    private final PaintingRepository paintingRepository;
+
+    private final MasterpieceRepository masterpieceRepository;
 
     private final WishlistRepository wishlistRepository;
+    private final PaintingRepository paintingRepository;
+
     public void postWishlist(int paintingId, String kakaoId){
 
         Member member = memberRepository.findByKakaoId(kakaoId);
 
-        Optional<Painting> painting = paintingRepository.findById(paintingId);
+        Optional<Masterpiece> masterpiece = masterpieceRepository.findById(paintingId);
 
         Wishlist wishlist = Wishlist.builder()
                             .member(member)
-                            .painting(painting.get())
+                            .masterpiece(masterpiece.get())
                             .build();
         wishlistRepository.save(wishlist);
     }
@@ -36,7 +45,42 @@ public class WishlistService {
     public void deleteWishlist(int paintingId, String kakaoId){
 
         Member member = memberRepository.findByKakaoId(kakaoId);
-        wishlistRepository.deleteByMember_IdAndPainting_Id(member.getId(),paintingId);
+        Optional<Masterpiece> masterpieceOptional = masterpieceRepository.findById(paintingId);
+        Masterpiece masterpiece = masterpieceOptional.get();
+
+//        Wishlist wishlist = wishlistRepository.findByMemberAndMasterpiece(member,masterpiece);
+
+//        System.out.println(member.getId()+"--------------------");
+//        System.out.println(masterpiece.getId()+"-------------------");
+//
+//        System.out.println(wishlist);
+
+        wishlistRepository.deleteByMemberAndMasterpiece(member,masterpiece);
+//        wishlistRepository.deleteById(wishlist.getId());
     }
+
+    public List<WishlistResponseDto> getWishList (String kakaoId){
+
+        List<WishlistResponseDto> res = new ArrayList<>();
+
+        Member member = memberRepository.findByKakaoId(kakaoId);
+
+        List<Wishlist> temp = wishlistRepository.findByMember(member);
+
+        for(int i = 0; i < temp.size(); i++){
+            WishlistResponseDto dto = new WishlistResponseDto();
+            Masterpiece Ent = temp.get(i).getMasterpiece();
+            dto.setPaintingId(Ent.getId());
+            dto.setTitle(Ent.getTitleKr());
+            dto.setImgSrc(Ent.getImgSrc());
+
+            res.add(dto);
+        }
+
+        return res;
+
+    }
+
+
 
 }
