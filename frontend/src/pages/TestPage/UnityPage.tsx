7a630a4f5376227d99702755c45d6d28d9f3b9e8
이동하 <siganshoyou@gmail.com>
2023-05-01 19:53:07 +0900

    [FEAT]유니티되는지확인

--- frontend/src/pages/TestPage/UnityPage.tsx
+++ frontend/src/pages/TestPage/UnityPage.tsx
@@ -1,6 +1,10 @@
-const Unity = () => {
-  return <div>
-    
-  </div>;
-};
-export default Unity;
+import Unity, { UnityContent } from "react-unity-webgl";
+export default function App() {
+    let unityContent = new UnityContent(
+        "unity/Build/edittset.json",
+        "unity/Build/UnityLoader.js"
+    );
+    return (
+        <div><Unity unityContent={unityContent} /></div>
+    );
+}
