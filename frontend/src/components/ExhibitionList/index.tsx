fcc3a0d0f20ab220352da07eadf064ecd56e6b78
haesom <fliamento@naver.com>
2023-05-04 17:51:44 +0900

    feat: exhibitionList페이지 작업중

--- frontend/src/components/ExhibitionList/index.tsx
+++ frontend/src/components/ExhibitionList/index.tsx
@@ -1,22 +1,50 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 
 import { exhibitionListActions } from "../../store/exhibitionListSlice";
+import { useHorizontalScroll } from "../utils/useSideScroll";
+
+import {
+  ImageContainer,
+  ImageStyle,
+  ImageTitleStyle,
+} from "../../styles/listStyles";
 
 const ExhibitionList = () => {
+  const scrollRef = useHorizontalScroll();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { data } = useSelector((state: RootState) => state.exhibitionList);
 
   useEffect(() => {
     dispatch(exhibitionListActions.getExhibitionListStart());
   }, []);
 
+  const goDetail = (id: number) => {
+    navigate(`/detail/${id}`);
+  };
+
   console.log("데이터는?", data);
 
   return (
     <div>
-      <h1>하이?</h1>
+      <ImageContainer ref={scrollRef}>
+        {data &&
+          data.map((item: any) => (
+            <ImageStyle key={item.id} onClick={() => goDetail(item.paintingId)}>
+              {
+                <img
+                  src={item.imgSrc}
+                  referrerPolicy="no-referrer"
+                  alt="전시회 이미지"
+                ></img>
+              }
+              <ImageTitleStyle>{item.titleOrigin}</ImageTitleStyle>
+            </ImageStyle>
+          ))}
+      </ImageContainer>
     </div>
   );
 };
