2f983338ffdab6e3ef183fadd22e555ace60a6fa
lj05117 <lj05117@naver.com>
2023-05-04 10:26:38 +0900

    feat: artList 페이지 가로 무한스크롤 구현

--- frontend/src/components/ArtList/index.tsx
+++ frontend/src/components/ArtList/index.tsx
@@ -3,19 +3,24 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect, useRef, EventHandler } from "react";
 import { useHorizontalScroll } from "../utils/useSideScroll";
+import { ListPageEnd } from "../List/styles";
 
 const ArtList = () => {
   const navigate = useNavigate();
   const [artListData, setArtListData] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(false);
   const pageEnd: any = useRef();
 
   let currentUrl = window.location.pathname.split("/");
+  const [prevPage, setPrevPage] = useState(0); //1로 하는게 맞는가?0으로 하는게 맞는가?
 
   useEffect(() => {
     setArtListData([]);
   }, []);
+  const loadMore = () => {
+    setPage((prev) => prev + 1);
+  };
 
   const getArtListDatas = async (page: number) => {
     const artListQuery = {};
@@ -27,7 +32,8 @@ const ArtList = () => {
       : "themes";
 
     const res: any = await axios.get(
-      `http://k8d203.p.ssafy.io:1234/api/paintings/${artListUrl}/${currentUrl[3]}`,
+      process.env.REACT_APP_API_BASE_URL +
+        `/paintings/${artListUrl}/${currentUrl[3]}/?page=${page + 1}`,
       {
         headers: {
           Authorization: accessToken,
@@ -36,13 +42,39 @@ const ArtList = () => {
     );
     console.log(res);
     const data = await res.data.content;
-    setArtListData(data);
+    if (data.length === 0) {
+      if (page === 0) {
+        setArtListData(data); // 검색결과가 없는 경우
+      }
+    } else {
+      if (page > prevPage) {
+        setArtListData((prev) => [...prev, ...data] as any);
+        setPrevPage(page);
+      } else {
+        setArtListData(data);
+      }
+    }
+    setLoading(true);
   };
 
   useEffect(() => {
     getArtListDatas(page);
   }, [page]);
-
+  useEffect(() => {
+    if (loading) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            loadMore();
+          }
+        },
+        {
+          threshold: 1,
+        }
+      );
+      observer.observe(pageEnd.current);
+    }
+  }, [loading]);
   const goDetail = (id: number) => {
     alert("이동하게 하기" + id);
     navigate(`/detail/${id}`);
@@ -64,6 +96,7 @@ const ArtList = () => {
             )}
           </ImageStyle>
         ))}
+        <ListPageEnd ref={pageEnd}></ListPageEnd>
       </ImageContainer>
     </ArtListContainer>
   );
