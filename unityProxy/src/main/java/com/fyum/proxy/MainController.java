478eae8a76675ff19caa5b211bb107a8fdf170bc
이동하 <siganshoyou@gmail.com>
2023-05-04 12:31:14 +0900

    [FEAT]unityPRoxybuild

--- unityProxy/src/main/java/com/fyum/proxy/MainController.java
+++ unityProxy/src/main/java/com/fyum/proxy/MainController.java
@@ -1,8 +1,6 @@
 package com.fyum.proxy;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Base64;
@@ -15,8 +13,8 @@ public class MainController {
     String test() {
         return "Hello World!";
     }
-    @GetMapping("/urlToBase64")
-    String urlToBase64(String url) {
+    @PostMapping("/urlToBase64")
+    String urlToBase64(@RequestParam("url") String url) {
         RestTemplate restTemplate = new RestTemplate();
         byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
         return Base64.getEncoder().encodeToString(imageBytes);
