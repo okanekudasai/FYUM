eef544f10b2dbbb88f84316874f325c4aaa23d0e
이동하 <siganshoyou@gmail.com>
2023-05-04 15:22:27 +0900

    [FEAT]proxyUpdate

--- unityProxy/src/main/java/com/fyum/proxy/MainController.java
+++ unityProxy/src/main/java/com/fyum/proxy/MainController.java
@@ -14,9 +14,14 @@ public class MainController {
         return "Hello World!";
     }
     @PostMapping("/urlToBase64")
-    String urlToBase64(@RequestParam("url") String url) {
-        RestTemplate restTemplate = new RestTemplate();
-        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-        return Base64.getEncoder().encodeToString(imageBytes);
+    String[] urlToBase64(@RequestParam("url") String url) {
+        String [] urlArray = url.split(",");
+        String [] baseArray = new String [urlArray.length];
+        for (int i = 0; i < urlArray.length; i++) {
+            RestTemplate restTemplate = new RestTemplate();
+            byte[] imageBytes = restTemplate.getForObject(urlArray[i], byte[].class);
+            baseArray[i] = Base64.getEncoder().encodeToString(imageBytes);
+        }
+        return baseArray;
     }
 }
