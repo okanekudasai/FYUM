e7fae22d357b1ed2f17c0be554f3c40b57f1dbd3
이동하 <siganshoyou@gmail.com>
2023-05-08 14:26:18 +0900

    [FEAT]unityReactTest

--- frontend/src/pages/TestPage/UnityPage.tsx
+++ frontend/src/pages/TestPage/UnityPage.tsx
@@ -1,10 +1,19 @@
 import Unity, { UnityContent } from "react-unity-webgl";
+import React, {useCallback, useEffect} from "react"  // 여기 임포트 할 거 있다는거에 주의
+
 export default function App() {
     let unityContent = new UnityContent(
         "unity/Build/edittset.json",
         "unity/Build/UnityLoader.js"
     );
+    function clicking() {
+        alert("눌렸음!");
+        unityContent.send("React", "aaa");
+    }
     return (
-        <div><Unity unityContent={unityContent} /></div>
+        <div>
+            <Unity unityContent={unityContent} />
+            <button onClick={clicking}>눌르셈</button>
+        </div>
     );
 }
