93f68f541d330d095a30e7dd7f04b273870d6902
이동하 <siganshoyou@gmail.com>
2023-05-08 14:42:42 +0900

    [FIX]buildUnity

--- frontend/src/pages/TestPage/UnityPage.tsx
+++ frontend/src/pages/TestPage/UnityPage.tsx
@@ -1,7 +1,7 @@
 import Unity, { UnityContent } from "react-unity-webgl";
 import React, {useCallback, useEffect} from "react"  // 여기 임포트 할 거 있다는거에 주의
 
-export default function Unity() {
+export default function App() {
     let unityContent = new UnityContent(
         "unity/Build/edittset.json",
         "unity/Build/UnityLoader.js"
