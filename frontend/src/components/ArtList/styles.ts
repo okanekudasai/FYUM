fbdd80cae38631c085b363f828e6e86fa0199f7d
lj05117 <lj05117@naver.com>
2023-05-09 16:10:36 +0900

    design: 사이드바 용 아이콘 생성

--- frontend/src/components/ArtList/styles.ts
+++ frontend/src/components/ArtList/styles.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 import BackImg from "../../assets/images/artListBackground.png";
+import { ReactComponent as CloseIc } from "../../assets/icon/closeIc.svg";
+import { grey } from "../../styles/colors";
 
 export const ArtListContainer = styled.div`
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   background-image: url(${BackImg});
+  @media screen and (max-width: 768px) {
+    position: absolute;
+    top: -3%;
+    height: 103%;
+  }
+`;
+
+export const SideBarCloseIcStyle = styled(CloseIc)`
+  height: 20px;
+  width: 20px;
+  fill: ${grey[300]};
 `;
