86f4fd3e36ec2e73acb918cfa455a88d0ec6ffe0
haesom <fliamento@naver.com>
2023-05-09 14:21:49 +0900

    feat: loading 컴포넌트 추가(lottie install)

---
+++ frontend/src/components/common/Loading/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LottieContainer = styled.div`
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
