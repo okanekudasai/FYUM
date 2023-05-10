7277509d2258521f15601cef72aba125c3833427
이동하 <siganshoyou@gmail.com>
2023-05-11 08:49:52 +0900

    [FEAT]만드는 글자수 1/3으로 줄임

--- unityProxy/src/main/java/com/fyum/proxy/MainController.java
+++ unityProxy/src/main/java/com/fyum/proxy/MainController.java
@@ -97,7 +97,7 @@ public class MainController {
         requestBody.put("model","text-davinci-003");
         requestBody.put("prompt", "I will give you the factors and probabilities for the picture, so please write an analysis." + gogogo);
         requestBody.put("temperature", 1.0f);
-        requestBody.put("max_tokens", 3000);
+        requestBody.put("max_tokens", 1000);
 
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
 
