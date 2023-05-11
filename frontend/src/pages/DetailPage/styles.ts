00a46edbcb6fb25e145fa7973bfc7bf958cc48e9
ahyunnnnnn <ahyun3089@naver.com>
2023-05-11 17:34:29 +0900

    feat: speaker css 수정
    Former-commit-id: a8e1b34e52b07c1979987aa9c0cbea50ba492f45

--- frontend/src/pages/DetailPage/styles.ts
+++ frontend/src/pages/DetailPage/styles.ts
@@ -119,7 +119,6 @@ export const DetailDiv = styled.div`
 `;
 
 export const ContentDiv = styled.div`
-  position: absolute;
   width: 90%;
   height: 160%;
   overflow-y: auto;
@@ -143,6 +142,11 @@ export const ContentDiv = styled.div`
   }
 `;
 
+export const SpeakerDiv = styled.div`
+  position: relative;
+  margin-top: 2%;
+`;
+
 export const Content = styled.p`
   font-size: 1vw;
   font-family: "SUIT";
@@ -159,8 +163,8 @@ export const Content = styled.p`
 `;
 
 export const SpeakerImg = styled(Speaker)`
-  width: 3vw;
-  height: 3vw;
+  width: 2vw;
+  height: 2vw;
   fill: ${white};
 
   @media (hover: hover) {
@@ -172,9 +176,10 @@ export const SpeakerImg = styled(Speaker)`
 `;
 
 export const MuteIcStyle = styled(MuteIc)`
-  width: 3vw;
-  height: 3vw;
+  width: 2vw;
+  height: 2vw;
   fill: ${white};
+  top: 5vh;
 
   @media (hover: hover) {
     &:hover {
