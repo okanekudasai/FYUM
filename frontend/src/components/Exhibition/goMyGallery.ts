01f1e1488365b445503558f0d5918ea13be0ec4c
lj05117 <lj05117@naver.com>
2023-05-12 10:58:54 +0900

    fix: 유니티 페이지 uri 수정
    Former-commit-id: 5bcf2449de252adaa6c430df01b29bc649add44a

--- frontend/src/components/Exhibition/goMyGallery.ts
+++ frontend/src/components/Exhibition/goMyGallery.ts
@@ -1,10 +1,7 @@
 export const goMyGallery = (gallerycode: Number, userToken: String) => {
   window.location.replace(
     process.env.REACT_APP_API_BASE_URL +
-      `/unityPage?num=` +
-      { gallerycode } +
-      `&token=` +
-      { userToken }
+      `/unityPage?num=${gallerycode}&token=${userToken}`
   );
   return undefined;
 };
