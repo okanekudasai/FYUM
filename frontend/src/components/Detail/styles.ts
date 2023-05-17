9ba47b23f872f7c2ab95dc49a24545dc41657178
haesom <fliamento@naver.com>
2023-05-17 09:49:16 +0900

    design: deleteIc 크기 수정
    Former-commit-id: e8aa894bb34a4adc261c048e75aac72395c7fbfa

--- frontend/src/components/Detail/styles.ts
+++ frontend/src/components/Detail/styles.ts
@@ -5,7 +5,8 @@ import { ReactComponent as DeleteIc } from "../../assets/icon/deleteIc.svg";
 
 export const DeleteIcStyle = styled(DeleteIc)`
   fill: ${grey[100]};
-  width: 1.6vw;
+  width: 27px;
+  height: 27px;
   align-self: center;
 
   @media (hover: hover) {
@@ -16,6 +17,7 @@ export const DeleteIcStyle = styled(DeleteIc)`
   }
 
   @media (max-width: 768px) {
-    width: 3.5vw;
+    width: 22px;
+    height: 22px;
   }
 `;
